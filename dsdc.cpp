7b15e25839be209fa506e74fe853ec09694ad2f5
Souryaansh <souryaansh.11@gmail.com>
2020-04-20 19:29:22 +0530

    first commit

---
+++ dsdc.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+map<int,unordered_set<int>> inp;
+map<int,unordered_set<int>> rev_inp;
+map<int,bool> p_visit;
+map<int,bool> d_visit;
+map<int,bool> main_visit;
+set<int> scc ;
+void dfs(int s, bool swit){
+    if(!swit){
+    d_visit[s] = true;
+        for(auto i : inp[s]){
+            if(!d_visit[i])dfs(i,swit);
+        }
+    }
+    if(swit){
+    p_visit[s] = true;
+        for(auto i : rev_inp[s]){
+            if(!p_visit[i] && d_visit[i])dfs(i,swit);
+            if(p_visit[i] && d_visit[i] ){if(scc.find(i)==scc.end())
+            // cout<<i<<" ";
+            scc.insert(i);
+            main_visit[i]=1;}
+        }
+    }
+    return ;
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for(int i=0;i<m;i++){
+        int a ,b ;
+        cin >> a >> b;
+        inp[a].insert(b);
+        rev_inp[b].insert(a);
+        inp[a].insert(a);
+        rev_inp[b].insert(b);
+        inp[b].insert(b);
+        rev_inp[a].insert(a);
+    }
+        // cout<<"hello";
+    // main_visit.assign(n,false);
+    int max_nodes =0 ;
+    for(auto i : inp){
+        scc.clear();
+        p_visit.clear();
+        d_visit.clear();
+        if(!main_visit[i.first]){
+            dfs(i.first,0);
+            dfs(i.first,1);
+            int count = scc.size();
+            max_nodes = max(count,max_nodes);
+            // if(scc.size()>0)cout<<"\n";
+    }}
+    cout <<"\n"<< max_nodes << endl;
+}
---
+++ kj.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+vector<bool> visited;
+stack<int> strong;
+int current =0;
+void dfs(int vertex,vector<int> graph[]){
+    visited[vertex] = true;
+    // cout<<vertex<<" ";
+    for(auto i : graph[vertex]){
+        if(!visited[i])dfs(i,graph);
+    }
+    strong.push(vertex);
+}
+void revdfs(int vertex,vector<int> graph[]){
+    visited[vertex] = true;
+    // cout<<vertex<<" ";
+    ++current;
+    for(auto i : graph[vertex]){
+        if(!visited[i])revdfs(i,graph);
+    }
+    strong.push(vertex);
+}
+
+int main(){
+    int v,e;
+    cin>>v>>e;
+    vector<int> graph[v];
+    vector<int> rev_graph[v];
+    for(int i=0;i<e;i++){
+        int inpv1,inpv2;
+        cin>>inpv1>>inpv2;
+        graph[inpv1].push_back(inpv2);
+    }
+    for(int i=0;i<v;i++)visited.push_back(false);
+    for(int i=0;i<v;i++){
+        if(!visited[i])dfs(i,graph);
+    }
+    for(int i=0;i<v;i++){
+        for(auto j:graph[i]){
+            rev_graph[j].push_back(i);
+        }
+    }
+    int maxtore=0;
+    current=0;
+    for(int i=0;i<v;i++){visited[i] = false;}
+    while(!strong.empty()){
+        current =0;
+        int curr = strong.top();
+        if(!visited[curr]){revdfs(curr,rev_graph);}
+        // cout<<"\n";
+        strong.pop();
+        maxtore = max(current,maxtore);
+    }
+    cout<<maxtore;
+
+}
